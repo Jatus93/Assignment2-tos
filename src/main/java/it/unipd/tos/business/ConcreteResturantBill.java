655b48f6bdb10d77701664834ccf27c4f27c3fb8
Jatus <gianmarco.pettinato@gmail.com>
2018-12-13 14:28:14 +0100

    closes #2 Implementato sconto con test

--- src/main/java/it/unipd/tos/business/ConcreteResturantBill.java
+++ src/main/java/it/unipd/tos/business/ConcreteResturantBill.java
@@ -13,8 +13,22 @@ public class ConcreteResturantBill implements ResturantBill {
     @Override
     public double getOrderPrice(List<MenuItem> itemsOrdered) throws RestaurantBillException {
         double result = 0;
+        int pizzaCounter = 0;
+        double cheaperPizza = 0;
         for(MenuItem item : itemsOrdered) {
             result += item.getPrice();
+            if(item.getType() == MenuItem.TypeElement.Pizza) {
+                pizzaCounter++;
+                if(cheaperPizza == 0) {
+                    cheaperPizza = item.getPrice();
+                }
+                if(cheaperPizza > item.getPrice()) {
+                    cheaperPizza = item.getPrice();
+                }
+            }
+        }
+        if(pizzaCounter >= 10) {
+            result-=cheaperPizza;
         }
         return result;
     }
--- src/test/java/it/unipd/tos/business/ConcreteResturantBillTest.java
+++ src/test/java/it/unipd/tos/business/ConcreteResturantBillTest.java
@@ -27,5 +27,21 @@ public class ConcreteResturantBillTest extends ConcreteResturantBill {
         order.add(new MenuItem(MenuItem.TypeElement.Primo,"Amatriciana",7.00));
         assertEquals(24.50, bill.getOrderPrice(order),0);
     }
+    @Test
+    public void testGetOrerPriceDiscountCheaperPizza() throws RestaurantBillException {
+        List<MenuItem> order = new ArrayList<MenuItem>();
+        order.add(new MenuItem(MenuItem.TypeElement.Pizza,"Marinara",4.00));
+        order.add(new MenuItem(MenuItem.TypeElement.Pizza,"Diavola",6.00));
+        order.add(new MenuItem(MenuItem.TypeElement.Pizza,"QuattroFormaggi",8.00));
+        order.add(new MenuItem(MenuItem.TypeElement.Pizza,"QuattroStagioni",7.00));
+        order.add(new MenuItem(MenuItem.TypeElement.Pizza,"Margherita",4.50));
+        order.add(new MenuItem(MenuItem.TypeElement.Pizza,"Diavola",6.00));
+        order.add(new MenuItem(MenuItem.TypeElement.Pizza,"Viennese",5.00));
+        order.add(new MenuItem(MenuItem.TypeElement.Pizza,"QuattroFormaggi",8.00));
+        order.add(new MenuItem(MenuItem.TypeElement.Pizza,"QuattroStagioni",7.00));
+        order.add(new MenuItem(MenuItem.TypeElement.Pizza,"Margherita",4.50));
+        order.add(new MenuItem(MenuItem.TypeElement.Pizza,"Diavola",6.00));
+        assertEquals(62,bill.getOrderPrice(order),0);
+    }
 
 }
